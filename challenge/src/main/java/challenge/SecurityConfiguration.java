82bb1cd0b84c35e0aab48c7e45eba80bf6c7fe5a
Parth <itsparth20@gmail.com>
2017-04-13 22:46:29 -0700

    Added HTTP Basic authentication for every end point

--- challenge/src/main/java/challenge/SecurityConfiguration.java
+++ challenge/src/main/java/challenge/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package challenge;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,14 +16,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
             .antMatchers("/h2-console/*").permitAll()
 //            .antMatchers("/user").permitAll()
-            .antMatchers("/user/*").permitAll()
-            .antMatchers("/user/*/connections").permitAll()
-            .antMatchers("/user/*/follow/*").permitAll()
-            .antMatchers("/user/*/unfollow/*").permitAll()            
-            .antMatchers("/user/*/distance/*").permitAll()
-            .anyRequest().authenticated();
+            .antMatchers("/user/*").hasRole("USER")
+            .antMatchers("/user/*/connections").hasRole("USER")
+            .antMatchers("/user/*/follow/*").hasRole("USER")
+            .antMatchers("/user/*/unfollow/*").hasRole("USER")          
+            .antMatchers("/user/*/distance/*").hasRole("USER")
+            .and()
+            .formLogin()
+            .permitAll().and().logout();
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+    	auth.inMemoryAuthentication()
+    		.withUser("user").password("password").roles("USER");
     }
 }
